73c422e384665ee9c833f51350e8e426d299812d
Alex Hughes <ahughesalex@gmail.com>
2018-07-28 01:03:00 +0000

    Create _lexer.py

---
+++ _lexer.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+
+def lex(characters, token_exprs):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in token_exprs:
+            pattern, name, tag = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tag:
+                    token = (name, text, tag)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character: %s\\n' % characters[pos])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
