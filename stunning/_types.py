bdf534fe21835c12170d592f752ac2b3a59a4b28
Alex Hughes <ahughes@d2.com>
2018-08-01 16:03:40 -0700

    Adding constants for the token identifier types

---
+++ stunning/_types.py
@@ -0,0 +1,13 @@
+# Reserved is a generic keyword type.
+# Meant to be a builtin for the TCL syntax.
+RESERVED = "RESERVED"
+
+# Ignore is a flag for characters that don't have functional impact on the TCL source.
+# We are using it quite liberally to ignore all whitespace and newline characters.
+IGNORE = "IGNORE"
+
+# Int is a flag applied on numeric tokens.
+INT = "INT"
+
+# Id is a flag applied on word tokens.
+ID = "ID"
