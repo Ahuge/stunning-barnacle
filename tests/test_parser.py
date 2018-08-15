816b02342305c611e48c6fc295fc501ec89baa89
Alex Hughes <ahughes@d2.com>
2018-08-15 10:46:38 -0700

    Adding a new testcase for animated knob values.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,7 @@
 import unittest
 
 from stunning import parser
+from stunning.exceptions import KeyFrameError
 
 from tests.test_utils import BaseTimingTest
 
@@ -54,6 +55,17 @@ Grade {
 }"""
 
 
+t2 = """Grade {
+ white {{curve x1001 1 x1006 2}}
+ black_clamp false
+ name Grade4
+ selected true
+ xpos 6102
+ ypos 2065
+}
+"""
+
+
 class ParserTestCase(BaseTimingTest):
     @BaseTimingTest.timing
     def test_simple(self):
@@ -73,6 +85,15 @@ class ParserTestCase(BaseTimingTest):
         self.assertEqual(nodes[2].knobs["white_panelDropped"], True)
         self.assertEqual(nodes[2].knobs["white"].r, 1)
         self.assertEqual(nodes[2].knobs["white"], (1, 0.808261, 0.460907, 1))
+
+    @BaseTimingTest.timing
+    def test_animated_value(self):
+        nodes = parser.parse(t2)
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].Class, "Grade")
+        self.assertEqual(nodes[0].knobs["white"].valueAt(1001), 1)
+        self.assertEqual(nodes[0].knobs["white"].valueAt(1006), 2)
+        self.assertRaises(nodes[0].knobs["white"].valueAt(1003), KeyFrameError)
 
 
 if __name__ == "__main__":
